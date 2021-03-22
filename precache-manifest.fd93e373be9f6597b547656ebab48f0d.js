8405952f5d5b127d18e2eef97097e70164078964
Yaro <yaroslav.sklema@gmail.com>
2021-03-22 15:07:00 +0200

    Updates

---
+++ precache-manifest.fd93e373be9f6597b547656ebab48f0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "306be99c4104315da14ee9410a246faa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4f6d6009a1cec752d4f8",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "5e7a27b3b34163807083",
+    "url": "./static/js/2.c1df2527.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c1df2527.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f6d6009a1cec752d4f8",
+    "url": "./static/js/main.74c02eb2.chunk.js"
+  },
+  {
+    "revision": "2880ca0324363234e9c2",
+    "url": "./static/js/runtime-main.b2ddd361.js"
+  }
+]);
